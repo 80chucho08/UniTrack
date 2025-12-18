086a30c7cd910dc81ab6988af311b0455db8123a
80chucho08 <chucho2004carbajal@gmail.com>
2025-12-17 23:31:35 -0600

    feat: connection for database

---
+++ backend/src/config/db.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+const testConnection = async () => {
+    try {
+        const [rows] = await pool.query("SELECT 1 + 1 AS RESULT");
+        console.log("Conexion creada", rows);
+    }catch (error){
+        console.log("error al conectar bd", error);
+    }
+}
+
+module.exports = { pool, testConnection };
