25122e7bcdbbffedc0d024239467a561525a2049
80chucho08 <chucho2004carbajal@gmail.com>
2025-12-19 23:17:11 -0600

    feat: agregar la ruta para auth en app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
 // Middleware para leer JSON
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 // Ruta de prueba
 app.get("/", (req, res) => {
