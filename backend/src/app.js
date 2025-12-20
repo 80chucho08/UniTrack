62001198351fe5a7ac629ddc350b5c50ce897ce6
80chucho08 <chucho2004carbajal@gmail.com>
2025-12-20 17:54:56 -0600

    feat: add cors to back for rutes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Middleware para leer JSON
 app.use(express.json());
