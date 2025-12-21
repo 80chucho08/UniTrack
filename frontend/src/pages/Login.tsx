980bef87ba2055d2b8e4cf4f654bd9e4d3d01ad6
80chucho08 <chucho2004carbajal@gmail.com>
2025-12-20 23:16:45 -0600

    fix: cambio en login para usar la ruta privada

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -13,12 +13,12 @@ interface LoginResponse {
     };
 }
 
-function Login(){ 
-    const [email, setEmail]  = useState("");
+function Login() {
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
-    const [loading, setLoading] =  useState(false);
-    
+    const [loading, setLoading] = useState(false);
+
     const { login } = useContext(AuthContext);
     const navigate = useNavigate();
 
@@ -29,12 +29,12 @@ function Login(){
         setLoading(true);
 
         try {
-            const response = await fetch("http://localhost:3000/api/auth/login",  {
+            const response = await fetch("http://localhost:3000/api/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({ email, password})
+                body: JSON.stringify({ email, password })
             });
 
             if (!response.ok) {
@@ -49,7 +49,7 @@ function Login(){
             navigate("/");
 
 
-        }catch (err) {
+        } catch (err) {
             setError("Email o contaseña incorrectos");
         } finally {
             setLoading(false);
@@ -58,33 +58,33 @@ function Login(){
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <form 
+            <form
                 onSubmit={handleSubmit}
-                className="bg-white p-6 rounded-xl shadow-md w-full max-w-sm"    
+                className="bg-white p-6 rounded-xl shadow-md w-full max-w-sm"
             >
                 <h2 className="text-2xl font-bold  mb-6 text-center">Iniciar Sesión</h2>
                 {error && (<p className="text-red-500 text-sm mb-4 text-center">{error}</p>)}
 
                 <div className="mb-4">
                     <label className="block text-sm font-medium mb-1">Email</label>
-                    <input 
+                    <input
                         className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
-                        type="email" 
-                        value={email} 
-                        onChange={(e) => 
-                        setEmail(e.target.value)} 
-                        required 
+                        type="email"
+                        value={email}
+                        onChange={(e) =>
+                            setEmail(e.target.value)}
+                        required
                     />
                 </div>
 
                 <div className="mb-6">
                     <label className="block text-sm font-medium mb-1">Contraseña</label>
-                    <input 
+                    <input
                         className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        required  
+                        required
                     />
                 </div>
 
@@ -92,10 +92,22 @@ function Login(){
                     className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition disabled:opacity-50"
                     type="submit"
                     disabled={loading}
-                    >
-                        {loading ? "Ingresando..." : "Entrar"}
-                    </button>
+                >
+                    {loading ? "Ingresando..." : "Entrar"}
+                </button>
+                <div>
+                    <p className="mt-4 text-center text-sm text-gray-600">
+                        ¿No tienes una cuenta?{" "}
+                        <a
+                            href="/register"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Regístrate
+                        </a>
+                    </p>
+                </div>
             </form>
+
         </div>
     );
 }
