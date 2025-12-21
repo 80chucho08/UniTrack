b544e1d8a39949d92751a2eb40e2d3d275e08f6c
80chucho08 <chucho2004carbajal@gmail.com>
2025-12-20 19:43:07 -0600

    feat: route de users en app.js del back

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cors({
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 // Ruta de prueba
 app.get("/", (req, res) => {
