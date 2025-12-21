a4b47b394793d78209dd9339fdca3cd24f5ae3d0
80chucho08 <chucho2004carbajal@gmail.com>
2025-12-20 23:15:03 -0600

    feat: agregar ruta para register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./routes/PrivateRoute";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         {/* Rutas públicas */}
         <Route path="/login" element={<Login />} />
-
+        <Route path="/register" element={<Register />} />
         {/* Rutas protegidas */}
         <Route element={<PrivateRoute />}>
           <Route path="/" element={<Dashboard />} />
