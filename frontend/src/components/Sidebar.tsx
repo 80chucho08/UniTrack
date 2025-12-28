3fb43a561705378072623d389cb1ddf176410649
80chucho08 <chucho2004carbajal@gmail.com>
2025-12-27 18:11:25 -0600

    feat: sidebar component

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -10,5 +10,25 @@ export function Sidebar () {
     const handleLogout = () => {
         logout();
         navigate("/login");
-    }
+    };
+
+    return (
+        <aside className="bg-white p-6 rounded-xl shadow-md h-fit">
+            <h2 className="text-xl font-bold mb-2">
+                Bienvenido
+            </h2>
+            <p className="text-gray-500 text-sm mb-6">
+                {user?.name}
+            </p>
+            <p className="text-gray-500 text-sm mb-6">
+                {user?.email}
+            </p>
+
+            <button 
+                onClick={handleLogout}
+                className="w-full bg-red-600 text-white py-2 rounded-md hover:bg-red-700 transition">
+                Cerrar sesión
+            </button>
+        </aside>
+    );
 }
