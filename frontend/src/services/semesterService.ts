33ae47f618431c5bd391324874a65e0aaea72d11
80chucho08 <chucho2004carbajal@gmail.com>
2025-12-28 21:34:23 -0600

    feat: function para fetch de get semester y create semester con manejo de error basico

--- frontend/src/services/semesterService.ts
+++ frontend/src/services/semesterService.ts
@@ -4,3 +4,36 @@ export interface Semester {
     id: number;
     name: string;
 }
+
+export async function getSemesters(tokke: string): Promise<Semester[]> {
+    const res = await fetch(API_URL, {
+        headers: {
+            Authorization: `Bearer ${tokke}`
+        }
+    });
+
+    if (!res.ok) {
+        throw new Error('Error al obtener los semestres');
+    }
+
+    return res.json();
+}
+
+export async function createSemester(
+    name: string,
+    token: string
+): Promise<Semester> {
+    const res = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ name })
+    });
+
+    if (!res.ok){
+        throw new Error('Error al crear el semestre');
+    }
+    return res.json();
+}
