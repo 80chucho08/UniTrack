7a9eef3b305252495738a56760ec7d62e4a568cc
80chucho08 <chucho2004carbajal@gmail.com>
2025-12-29 17:24:10 -0600

    feat: funcion para get subject si la consulta todavia

--- backend/src/models/subject.model.js
+++ backend/src/models/subject.model.js
@@ -2,6 +2,10 @@ const { pool } = require("../config/db");
 
 const getSubjects = async(id, semesterId) => {
     const query = (``);
+
+    const [rows] = await pool.execute(query, [id, semesterId]);
+
+    return rows;
 }
 
 
