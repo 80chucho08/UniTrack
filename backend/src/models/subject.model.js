46a28907958a5f22c049c08250b7cd8a0789ef30
80chucho08 <chucho2004carbajal@gmail.com>
2025-12-31 18:33:41 -0600

    consulta getSubject corregida

--- backend/src/models/subject.model.js
+++ backend/src/models/subject.model.js
@@ -1,24 +1,28 @@
 const { pool } = require("../config/db");
 
-const getSubjects = async(id, semesterId) => {
-    const query = (`SELECT Sb.id, Sb.user_id, Sb.semester_id, Sb.name
-                    FROM user U 
-                    JOIN semesters S
-                        ON U.id = S.user_id
-                    JOIN subjects Sb
-                     ON S.user_id =  Sb.user_id`);
+const getSubjects = async (userId, semesterId) => {
+    const query = `
+        SELECT 
+            Sb.id, 
+            Sb.user_id, 
+            Sb.semester_id, 
+            Sb.name
+        FROM subjects Sb
+        INNER JOIN semesters S ON Sb.semester_id = S.id
+        WHERE Sb.user_id = ? AND Sb.semester_id = ?
+    `;
 
-    const [rows] = await pool.execute(query, [id, semesterId]);
+    const [rows] = await pool.execute(query, [userId, semesterId]);
 
     return rows;
 }
 
 
-const addSubject = async(userId, semesterId, subjectId) => {
+const addSubject = async (userId, semesterId, subjectId) => {
     const query = (``);
 
     const [result] = await pool.execute(query, []);
-    
+
     return result.insertId;
 
 }
