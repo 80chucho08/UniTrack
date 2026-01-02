c8d13f2a448a5b7db8212bad86137b0b36bb8426
80chucho08 <chucho2004carbajal@gmail.com>
2026-01-01 19:04:00 -0600

    feat: subject cretae Subject prueba

--- backend/src/controllers/subject.controller.js
+++ backend/src/controllers/subject.controller.js
@@ -5,9 +5,41 @@ const getSubjects = async (req, res) => {
         const userId = req.user.id;
         const { semesterId } = req.params;
 
+        if(!userId || !semesterId) {
+            return res.status(400).json({
+                message: "El id del usuario o el id de semestre es requerido"
+            });
+        }
+
         const subjects = await subjectModel.getSubjects(userId, semesterId);
         res.json(subjects);
     }catch(error){
-
+        res.status(500).json({ message: "Error al obtener materias" });
     }
 }
+
+const createSubject = async (req, res) => {
+    try{
+        const userId = req.user.id;
+        const { semesterId } = req.params;
+        if(!userId || !semesterId){
+            return res.status(400).json({
+                message: "El id del usuario o el id de semestre es requerido"
+            });
+        }
+
+        const subjectId = await subjectModel.addSubject(
+            userId,
+            semesterId,
+            req.body
+        );
+
+        res.status(201).json({
+            message: "Materia creada correctamente",
+            subjectId
+        });
+    }catch(error){
+        res.status(500).json({ message: "Error al crear materia" });
+    }
+}
+
