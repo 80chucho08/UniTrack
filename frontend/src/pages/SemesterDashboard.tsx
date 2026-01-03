f33ce3acdb6a430a88e5d2f35656c3e18854f570
80chucho08 <chucho2004carbajal@gmail.com>
2026-01-02 18:36:15 -0600

    feat: html para el dashboard con los dstos del back

--- frontend/src/pages/SemesterDashboard.tsx
+++ frontend/src/pages/SemesterDashboard.tsx
@@ -10,20 +10,20 @@ function SemesterDashboard() {
     const { semesterId } = useParams();
     const { token } = useContext(AuthContext);
 
-    const [Subjects, setSubjects] = useState<Subject[]>([]);
+    const [subjects, setSubjects] = useState<Subject[]>([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fecthSubjects = async () => {
             try {
-                if(!token || !semesterId) return;
+                if (!token || !semesterId) return;
 
                 const data = await getSubjects(
                     Number(semesterId),
                     token
                 );
                 setSubjects(data);
-            }catch(error){
+            } catch (error) {
                 console.error('Error al obtener materias: ', error);
             } finally {
                 setLoading(false);
@@ -36,8 +36,35 @@ function SemesterDashboard() {
     return (
         <div className="min-h-screen bg-gray-100 p-8">
             <h1 className="text-3xl font-bold mb-6">
-                Materias del Semestre
+                Materias del semestre
             </h1>
+
+            {loading && <p>Cargando materias...</p>}
+
+            {!loading && subjects.length === 0 && (
+                <p>No hay materias registradas.</p>
+            )}
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {subjects.map(subject => (
+                    <div
+                        key={subject.id}
+                        className="bg-white p-4 rounded-xl shadow"
+                    >
+                        <h2 className="font-semibold text-lg">
+                            {subject.name}
+                        </h2>
+
+                        {subject.teacher_name && (
+                            <p className="text-sm text-gray-500">
+                                Prof: {subject.teacher_name}
+                            </p>
+                        )}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
+
+export default SemesterDashboard;
