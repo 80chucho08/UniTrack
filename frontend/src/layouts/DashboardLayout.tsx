6610b6dd61539783a9bc6c7195c24a2ca9dc27da
80chucho08 <chucho2004carbajal@gmail.com>
2026-01-07 20:14:51 -0600

    feat: dashboard layout

---
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -0,0 +1,29 @@
+import { Outlet } from "react-router-dom";
+import { Sidebar } from "../components/Sidebar";
+import { AppHeader } from "../components/AppHeader";
+import { AppFooter } from "../components/AppFooter";
+
+export default function DashboardLayout() {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main area */}
+      <div className="flex-1 flex flex-col min-w-0">
+        
+        {/* Header global */}
+        <AppHeader />
+
+        {/* Page content */}
+        <main className="flex-1 overflow-y-auto">
+          <Outlet />
+        </main>
+
+        {/* Footer */}
+        <AppFooter />
+      </div>
+    </div>
+  );
+}
