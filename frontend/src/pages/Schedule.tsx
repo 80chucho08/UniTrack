b8f72e898c30fe03abc6309443703a8a38e33bc2
80chucho08 <chucho2004carbajal@gmail.com>
2026-01-11 21:29:52 -0600

    schedule basic

--- frontend/src/pages/Schedule.tsx
+++ frontend/src/pages/Schedule.tsx
@@ -1,10 +1,25 @@
-
+import { DashboardHeader } from "../components/DashboardHeader";
 
 const Schedule = () => {
     return (
-        <div>
-            Schedule Component
-        </div>
+        <>
+            <main className="p6 lg:p-10 max-w-7x1 w-full mx-auto">
+                <DashboardHeader
+                    title="Horario"
+                    subtitle={`Visualiza y organiza tu horario académico`}
+                >
+                    {/* Additional content or buttons can be added here */}
+                </DashboardHeader>
+
+                <div className="border">
+
+                </div>
+
+                <div>
+
+                </div>
+            </main>
+        </>
     )
 }
 
