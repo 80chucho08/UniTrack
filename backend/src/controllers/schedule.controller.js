7a5c121347de8cf1aafc540df9eb40136c3d3d99
80chucho08 <chucho2004carbajal@gmail.com>
2026-01-19 23:07:48 -0600

    feat: schedule model y controller

--- backend/src/controllers/schedule.controller.js
+++ backend/src/controllers/schedule.controller.js
@@ -1,1 +1,25 @@
 const scheduleModel = require("../models/schedule.model");
+
+const getSubjectsBySemester = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const { semesterId } = req.params;
+
+        if (!userId || !semesterId) {
+            return res.status(400).json({
+                message: "El id o el id de semester es requerido"
+            });
+        }
+
+        const subjects = await scheduleModel.getSubjectsBySemester(userId, semesterId);
+        res.json(subjects);
+    } catch (error) {
+        console.error("Error detallado:", error); // Esto aparecerá en tu terminal/VS Code
+        res.status(500).json({
+            message: "Error al obtener materias",
+            error: error.message // Esto te ayudará a ver el problema en Thunder Client
+        });
+    }
+}
+
+module.exports = { getSubjectsBySemester };
--- backend/src/models/schedule.model.js
+++ backend/src/models/schedule.model.js
@@ -1,1 +1,20 @@
-const {pool} = require("../config/db");
+const { pool } = require("../config/db");
+
+const getSubjectsBySemester = async (userId, semesterId) => {
+    const query = `SELECT 
+            Sb.id, 
+            Sb.user_id, 
+            Sb.semester_id, 
+            Sb.name,
+            Sb.color
+        FROM subjects Sb
+        INNER JOIN semesters S ON Sb.semester_id = S.id
+        WHERE Sb.user_id = ? AND Sb.semester_id = ?`;
+
+    const [rows] = await pool.execute(query, [userId, semesterId]);
+
+    return rows;
+}
+
+
+module.exports = { getSubjectsBySemester };
